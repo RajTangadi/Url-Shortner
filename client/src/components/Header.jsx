1582f9144a69dccfc114f666ef9d39ab196294f6
Raj Tangadi <rajtangadi65@gmail.com>
2025-04-02 16:31:08 +0530

    fix issue

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,28 @@
 import { FaSearch } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
-  const [searchTerm, setSearchTerm] = useState("");
-  const navigate = useNavigate();
+  // const [searchTerm, setSearchTerm] = useState("");
+  // const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set("searchTerm", searchTerm);
-    const searchQuery = urlParams.toString();
-    navigate(`/search?${searchQuery}`);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const urlParams = new URLSearchParams(window.location.search);
+  //   urlParams.set("searchTerm", searchTerm);
+  //   const searchQuery = urlParams.toString();
+  //   navigate(`/search?${searchQuery}`);
+  // };
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(location.search);
-    const searchTermFromUrl = urlParams.get("searchTerm");
-    if (searchTermFromUrl) {
-      setSearchTerm(searchTermFromUrl);
-    }
-  }, [location.search]);
+  // useEffect(() => {
+  //   const urlParams = new URLSearchParams(location.search);
+  //   const searchTermFromUrl = urlParams.get("searchTerm");
+  //   if (searchTermFromUrl) {
+  //     setSearchTerm(searchTermFromUrl);
+  //   }
+  // }, [location.search]);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -32,7 +32,7 @@ const Header = () => {
             <span className="text-slate-700">Shortner</span>
           </h1>
         </Link>
-        <form
+        {/* <form
           className="bg-slate-100 p-3 rounded-lg flex items-center"
           onSubmit={handleSubmit}
         >
@@ -46,7 +46,7 @@ const Header = () => {
           <button>
             <FaSearch className="text-slate-600" />
           </button>
-        </form>
+        </form> */}
         <ul className="flex gap-4">
           <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
